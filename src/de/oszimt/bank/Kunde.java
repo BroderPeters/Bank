2d14df9466d31208d54743c8c2a62086960b13ea
user <user@2304-S27>
2017-03-08 08:43:36 +0100

    Kunde erstellt

---
+++ src/de/oszimt/bank/Kunde.java
@@ -0,0 +1,28 @@
+package de.oszimt.bank;
+
+public class Kunde {
+
+	private String name;
+	private int kundennr;
+	
+	public Kunde(String name, int kundennr){
+		this.name = name;
+		this.kundennr = kundennr;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKundennr() {
+		return kundennr;
+	}
+
+	public void setKundennr(int kundennr) {
+		this.kundennr = kundennr;
+	}
+}
